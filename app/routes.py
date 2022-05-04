ccfad5acff197230331b2e22ebe61e3517bfffbd
unknown <rakkakeren@gmail.com>
2022-05-05 00:22:40 +0700

    initial commit

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from flask import request, render_template, redirect
+from app import app
+
+@app.route("/", methods=["GET"])
+def home():
+    if request.method == "GET":
+        athletes = ["Maradona", "Leon"]
+        return render_template("index.html", athletes=athletes)
---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+base_dir = os.path.dirname(__file__)
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+    ES_USER = ""
+    ES_PWD = ""
+    ES_SERVER = "https://localhost:9200"
+
+    MONGO_SERVER = "localhost:27017"
+    INDEX = "athlete"
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'jdlfjds7834kjfksdfhdsds'
+    
+
+class ProductionConfig(Config):
+    ...
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
